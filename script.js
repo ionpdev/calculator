fda4e5ca1b16803e395b40d0c929b14472c5135d
johny2433 <johny.codmw3@gmail.com>
2020-11-19 19:11:10 +0200

    reset button, and decimal button

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 const calculatorDisplay = document.querySelector("h1");
 const inputBtns = document.querySelectorAll("button");
-const clearBtn = document.getElementById("clear-btn");
+const clearBtn = document.getElementById("clear-button");
+
+//
 
 function sendNumberValue(number) {
   // if current display value is 0, replace it. if not add number
   const displayValue = calculatorDisplay.textContent;
   calculatorDisplay.textContent =
     displayValue === "0" ? number : displayValue + number;
+}
+
+//
+function addDecimal() {
+  // if no decimal, add one
+  if (!calculatorDisplay.textContent.includes(".")) {
+    calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`;
+  }
 }
 
 // Add event listeners for numbers, operators, decimal buttons
@@ -16,6 +26,14 @@ inputBtns.forEach((inputBtn) => {
   } else if (inputBtn.classList.contains("operator")) {
     inputBtn.addEventListener("click", () => sendNumberValue(inputBtn.value));
   } else if (inputBtn.classList.contains("decimal")) {
-    inputBtn.addEventListener("click", () => sendNumberValue());
+    inputBtn.addEventListener("click", () => addDecimal());
   }
 });
+
+// Reset Display
+function resetAll() {
+  calculatorDisplay.textContent = "0";
+}
+
+// Event Listener
+clearBtn.addEventListener("click", resetAll);
